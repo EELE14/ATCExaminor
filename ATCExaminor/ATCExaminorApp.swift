d5d7993094fc0152af941a30161646f21539123e
EELE <192506170+EELE14@users.noreply.github.com>
2026-04-16 17:39:22 +0200

    Initial Commit

---
+++ ATCExaminor/ATCExaminorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ATCExaminorApp.swift
+//  ATCExaminor
+//
+//  Created by Emanuel Lensing on 16.04.26.
+//
+
+import SwiftUI
+
+@main
+struct ATCExaminorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ATCExaminorTests/ATCExaminorTests.swift
@@ -0,0 +1,18 @@
+//
+//  ATCExaminorTests.swift
+//  ATCExaminorTests
+//
+//  Created by Emanuel Lensing on 16.04.26.
+//
+
+import Testing
+
+struct ATCExaminorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
